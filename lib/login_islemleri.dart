ab89f2d2b89db56b489283636bdaba21de710c64
MFA <53713274+mfabank@users.noreply.github.com>
2020-11-02 02:16:36 +0300

    User, Create-Login-LogOut-ForgotPassword example

---
+++ lib/login_islemleri.dart
@@ -0,0 +1,102 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+FirebaseAuth _auth = FirebaseAuth.instance;
+
+class LoginIslemleri extends StatefulWidget {
+  @override
+  _LoginIslemleriState createState() => _LoginIslemleriState();
+}
+
+class _LoginIslemleriState extends State<LoginIslemleri> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _auth
+        .authStateChanges()
+        .listen((User user) {
+      if (user == null) {
+        print('Kullanıcı oturumu kapattı');
+      } else {
+        print('Kullanıcı oturum açtı!');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login İşlemleri"),
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            RaisedButton(
+              child: Text("Email-Şifre Oluştur"),
+              color: Colors.blueAccent,
+              onPressed: _emailSifreKullaniciOlustur,
+            ),
+            RaisedButton(
+              child: Text("Email-Şifre Giriş"),
+              color: Colors.greenAccent,
+              onPressed: _emailSifreKullaniciGirisYap,
+            ),
+            RaisedButton(
+              child: Text("Çıkış Yap"),
+              color: Colors.red,
+              onPressed: _cikisYap,
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+
+  void _emailSifreKullaniciOlustur() async{
+    String _email = "muhammedfatihaktass@gmail.com";
+    String _password = "123456";
+
+    try{
+      UserCredential _credential = await _auth.createUserWithEmailAndPassword(email: _email, password: _password);
+      User _yeniUser = _credential.user;
+       await _yeniUser.sendEmailVerification();
+       if(_auth.currentUser != null){
+         debugPrint("Lütfen mail adresinizi onaylayın.");
+         _auth.signOut();
+         debugPrint("Kullanıcı sistemden atılıyor.");
+       }
+      debugPrint(_yeniUser.toString());
+    }
+    catch(e){
+      debugPrint("***************************OPS**********************");
+      debugPrint(e.toString());
+    }
+
+
+  }
+
+  void _emailSifreKullaniciGirisYap() async{
+    String _email = "muhammedfatihaktass@gmail.com";
+    String _password = "123456";
+    if(_auth.currentUser == null) {
+     UserCredential _credential = await _auth.signInWithEmailAndPassword(email: _email, password: _password);
+    }
+    else{
+      debugPrint("Şu an zaten giriş yapılmış");
+    }
+  }
+
+  void _cikisYap() async{
+    if(_auth.currentUser != null) {
+      await _auth.signOut();
+    }
+    else{
+      debugPrint("Zaten oturum açılmamış.");
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_example/login_islemleri.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("Hata Çıktı" + snapshot.error.toString()),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LoginIslemleri();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+}
