990d5c03befea52b3a0d6ad0783ff31bfcb99377
MFA <53713274+mfabank@users.noreply.github.com>
2020-11-07 22:06:40 +0300

    ForgotPassword,UpdateEmail,UpdatePassword,GoogleSıgnIN

--- lib/login_islemleri.dart
+++ lib/login_islemleri.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -40,6 +42,26 @@ class _LoginIslemleriState extends State<LoginIslemleri> {
               child: Text("Email-Şifre Giriş"),
               color: Colors.greenAccent,
               onPressed: _emailSifreKullaniciGirisYap,
+            ),
+            RaisedButton(
+              child: Text("Şifremi Unuttum"),
+              color: Colors.deepPurple,
+              onPressed: _resetPassword,
+            ),
+            RaisedButton(
+              child: Text("Şifremi Güncelle"),
+              color: Colors.pinkAccent,
+              onPressed: _updatePassword,
+            ),
+            RaisedButton(
+              child: Text("Email Güncelle"),
+              color: Colors.teal,
+              onPressed: _updateEmail,
+            ),
+            RaisedButton(
+              child: Image.network("https://img-authors.flaticon.com/google.jpg",width: 40,height: 40,),
+              color: Colors.teal,
+              onPressed: _signInWithGoogle,
             ),
             RaisedButton(
               child: Text("Çıkış Yap"),
@@ -103,6 +125,84 @@ class _LoginIslemleriState extends State<LoginIslemleri> {
       await _auth.signOut();
     } else {
       debugPrint("Zaten oturum açılmamış.");
+    }
+  }
+
+  void _resetPassword() async{
+    String _email = "muhammedfatihaktass@gmail.com";
+    try {
+      await _auth.sendPasswordResetEmail(email: _email);
+      debugPrint("Mail gönderildi");
+    }
+    catch(e) {
+      debugPrint("Şifre Sıfırlama Hatası");
+    }
+  }
+
+  void _updatePassword() async{
+    try {
+     await _auth.currentUser.updatePassword("123456");
+      debugPrint("Şifreniz Güncellendi");
+
+    }
+    catch(e) {
+
+      try{
+        String email = "muhammedfatihaktass@gmail.com";
+        String password = "123456";
+
+        EmailAuthCredential credential =
+        EmailAuthProvider.credential(email: (email), password: password);
+        await FirebaseAuth.instance.currentUser.reauthenticateWithCredential(credential);
+        await _auth.currentUser.updatePassword("password2");
+      }
+      catch(e){
+        debugPrint("Hata Çıktı");
+      }
+
+      debugPrint("Hatalı İşlem !!! : $e");
+    }
+  }
+
+  void _updateEmail() async{
+    try {
+      await _auth.currentUser.updateEmail("fatih@fatih.com");
+      debugPrint("Email Güncellendi");
+
+    }
+    catch(e) {
+
+      try{
+        String email = "muhammedfatihaktass@gmail.com";
+        String password = "password2";
+
+        EmailAuthCredential credential =
+        EmailAuthProvider.credential(email: (email), password: password);
+        await FirebaseAuth.instance.currentUser.reauthenticateWithCredential(credential);
+        await _auth.currentUser.updateEmail("fatih@fatih.com");
+      }
+      catch(e){
+        debugPrint("Hata Çıktı");
+      }
+
+      debugPrint("Hatalı İşlem !!! : $e");
+    }
+  }
+
+  Future<UserCredential> _signInWithGoogle() async{
+    try {
+
+      final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      return await _auth.signInWithCredential(credential);
+
+    }
+    catch(e) {
+      debugPrint("Hata var! $e");
     }
   }
 }
