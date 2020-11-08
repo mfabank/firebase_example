f16fa8f29a007b0db2ae5d6748eee3b309b2be00
MFA <53713274+mfabank@users.noreply.github.com>
2020-11-09 01:01:11 +0300

    CloudFirestore Examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_example/firestore_islemler.dart';
 import 'package:firebase_example/firestore_islemlerimiz.dart';
 import 'package:firebase_example/login_islemleri.dart';
 import 'package:flutter/material.dart';
